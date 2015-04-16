2bb42ee895597c834d7dfb31a0d85799932432a7
cekees <cekees@gmail.com>
2015-04-16 15:45:02 -0400

    added a module with a function and a test module

--- notebooks/fem_plt.py
+++ notebooks/fem_plt.py
@@ -1,3 +1,10 @@
-def contour_c0p1(vertices, element, C):
-    fig, ax = plt.subplots()
-    ax.tricontourf(vertices[:,0],vertices[:,1],elements,C)
+"""
+A module for plotting general finite element approximations
+"""
+
+def contourf_c0p1(ax,vertices, elements, u):
+    """
+    Filled contour plot  of scalar on 2D unstructured  mesh
+    """
+    ax.tricontourf(vertices[:,0],vertices[:,1],elements,u)
+    
